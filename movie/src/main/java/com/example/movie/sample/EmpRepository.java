2fb673e8108781fb299df6ead82370896b16fc0f
Manics7 <Manics7@192.168.0.100>
2021-12-16 03:36:11 +0900

    ㅇㅇ

--- movie/src/main/java/com/example/movie/sample/EmpRepository.java
+++ movie/src/main/java/com/example/movie/sample/EmpRepository.java
@@ -1,9 +1,6 @@
 package com.example.movie.sample;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.movie.dto.Emp;
 
 public interface EmpRepository extends JpaRepository<Emp, Integer> {
 
