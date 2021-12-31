bde966893f4d681dc4b700a5ec963b2f7f8cb8b0
Manics7 <Manics7@192.168.0.100>
2022-01-01 02:42:45 +0900

    0101

--- movie/src/main/java/com/example/movie/repository/TheaterRepository.java
+++ movie/src/main/java/com/example/movie/repository/TheaterRepository.java
@@ -3,8 +3,6 @@ package com.example.movie.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.movie.entity.MovieOfficial;
-import com.example.movie.entity.Question;
 import com.example.movie.entity.Theater;
 
 @Repository
