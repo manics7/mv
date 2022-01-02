ad936fc3c0fcf344cea25396c86bc8a6372611e7
ghddl <ghddl@192.168.219.103>
2022-01-03 00:51:41 +0900

    0103

--- movie/src/main/java/com/example/movie/dto/MemberDto.java
+++ movie/src/main/java/com/example/movie/dto/MemberDto.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class MemberDto {
 	private String m_id;
 	private String m_pw;
-	private String m_birth;
+	private Timestamp m_birth;
 	private String m_tel;
 	private String m_like;
 	private String m_email;
