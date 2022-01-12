fa1a258666cd33516f98e63122ca11fad4a56d12
dnckd <dnckd18@nate.com>
2022-01-12 20:49:30 +0900

    0112

--- movie/src/main/java/com/example/movie/repository/ScheduleDetailRepository.java
+++ movie/src/main/java/com/example/movie/repository/ScheduleDetailRepository.java
@@ -1,6 +1,8 @@
 package com.example.movie.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import com.example.movie.entity.ScheduleDetail;
 
 @Repository
 public interface ScheduleDetailRepository extends JpaRepository<ScheduleDetail, Integer>  {
+
+	List<ScheduleDetail> findBySchCode(Integer schCode);
 
 	
 }
